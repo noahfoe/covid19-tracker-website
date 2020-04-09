832da7ce836744609c7d62cee1587144ee902832
noahfoe <noahfoley6@gmail.com>
2020-04-09 17:05:43 -0500

    Updates

---
+++ precache-manifest.b0ca0d50c59651dd352cf0277ea57ae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440c9f3fcf566bfac5934d45ffe40537",
+    "url": "/covid19-tracker-website/index.html"
+  },
+  {
+    "revision": "97ef3f4ef5087f037bef",
+    "url": "/covid19-tracker-website/static/css/main.0004c80f.chunk.css"
+  },
+  {
+    "revision": "8982b0b02bca45a44321",
+    "url": "/covid19-tracker-website/static/js/2.7b378f3d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker-website/static/js/2.7b378f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ef3f4ef5087f037bef",
+    "url": "/covid19-tracker-website/static/js/main.57a77fb1.chunk.js"
+  },
+  {
+    "revision": "d20ee7aa40fbbc8c5dbd",
+    "url": "/covid19-tracker-website/static/js/runtime-main.ba7c6ccd.js"
+  },
+  {
+    "revision": "94863534c718d35997f6c96cf3441cb5",
+    "url": "/covid19-tracker-website/static/media/Image.94863534.png"
+  }
+]);
